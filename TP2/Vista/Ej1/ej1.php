d27cc08e65a224aa73bdd31d2d4b7c696476cc2c
Lautaro Gonzalez <lautarofglez@gmail.com>
2023-08-25 01:55:10 -0300

    Index básico para proyecto
    Agregué un index al proyecto para navegar por los distintos ejercicios de los tp 1 al 3

---
+++ TP2/Vista/Ej1/ej1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("form").submit(function(event) {
+                // Evita que el formulario se envíe automáticamente
+                event.preventDefault();
+
+                // Validación simple: verifica si los campos están vacíos
+                var name = $("#name").val();
+                var email = $("#email").val();
+
+                if (name === "" || email === "") {
+                    alert("Por favor, complete todos los campos.");
+                } else {
+                    alert("Formulario enviado correctamente.");
+                    // Aquí podrías agregar lógica adicional para enviar el formulario
+                }
+            });
+        });
+    </script>
+</head>
+<body>
+    <h1>Decidimos usar JQuery como framework de JS después de investigación de otros frameworks</h1>
+    <h3>Ejercicio 1: Investigue y pruebe la validación de formularios usando alguna librería o framework javaScript (JQuery, Mootools, Dojo, Prototype, etc).</h3>
+    <br><br>
+    <h5>Ejemplo:</h5>
+    <form>
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" name="name">
+        <br>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" name="email">
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <title>Grupo 1 PDW 2023</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Chakma&family=Roboto:ital@1&display=swap');
+        *{font-family: 'Noto Sans Chakma', sans-serif;
+        font-family: 'Roboto', sans-serif;}
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="container-fluid justify-content-center collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav"> 
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP1</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ1/ej1.php">Ejercicio 1</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ2/ej2.php">Ejercicio 3</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ3/ej3.php">Ejercicio 3</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ4/ej4.php">Ejercicio 4</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ5/ej5.php">Ejercicio 5</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ6/ej6.php">Ejercicio 6</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ7/ej7.php">Ejercicio 7</a></li>
+            <li><a class="dropdown-item" href="./TP1/Vista/Vista-EJ8/ej8.php">Ejercicio 8</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP2</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="./TP2/Vista/Ej1/ej1.php">Ejercicio 1</a></li>
+                <li><a class="dropdown-item" href="./TP2/Vista/Ej2/ej2.php">Ejercicio 2</a></li>
+                <li><a class="dropdown-item" href="./TP2/Vista/Ej3/ej3.php">Ejercicio 3</a></li>
+                <li><a class="dropdown-item" href="./TP2/Vista/Ej4/ej4.php">Ejercicio 4</a></li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">TP3</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="./TP3/Vista/Ej1/ej1.php">Ejercicio 1</a></li>
+                <li><a class="dropdown-item" href="./TP3/Vista/Ej2/ej2.php">Ejercicio 2</a></li>
+                <li><a class="dropdown-item" href="./TP3/Vista/EJ3/ej3.php">Ejercicio 3</a></li>
+            </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<main class="d-flex justify-content-center align-items-center">
+    <div class="container text-center">
+        <h2>Integrantes del grupo 1</h2>
+        <ul class="list-unstyled">
+            <li>Lautaro Gonzalez FAI-2950</li>
+            <li>Emiliano Lopez FAI-3357</li>
+            <li>Valentin Camusso FAI-3208</li>
+        </ul>
+    </div>
+</main>
+</body>
+</html>
