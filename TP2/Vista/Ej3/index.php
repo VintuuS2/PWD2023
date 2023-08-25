9f23ace208eba8d302d0764e26d7f796886f22b2
Emiliano Lopez FAI [3357] <emiliano.lopez@est.fi.uncoma.edu.ar>
2023-08-24 22:26:02 -0300

    Subo tp2 modificacion del c)

--- TP2/Vista/Ej3/index.php
+++ TP2/Vista/Ej3/index.php
@@ -5,7 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ej3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
     <script>
         $(document).ready(function() {
             $("form").submit(function(event) {
@@ -37,14 +41,36 @@
     </script>
 </head>
 
-<body>
-    <form action="./Accion/a_alta_usuario.php" method="post" onsubmit="return validar();">
-        Usuario: <input type="text" name="usuario" id="usuario">
-        <br>
-        Password: <input type="password" name="contra" id="contra">
-        <input type="submit" value="Enviar">
-    </form>
-    <a href="./Accion/a_alta_usuario.php">Accion</a>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="form-signin w-100 m-auto">
+        <div class="container-centered">
+            <form action="../../Vista/Ej3/Accion/a_alta_usuario.php" method="post" id="form">
+                <div class="container d-flex justify-content-center">
+                    <div class="bg-secondary m-4  d-flex justify-content-center align-items-center" style="height: 300px; width: 300px;">
+                        <div class="bg-light position-relative " style="height: 270px; width: 270px;">
+                            <div class="d-grid gap-2 col-10 mx-auto position-absolute top-50 start-50 translate-middle">
+                                <p class="h5 text-center">Member Login</p>
+
+                                <div class="form-floating mb-2">
+                                    <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Username">
+
+                                    <label for="usuario" id="mensaje1"><i class="bi bi-person-fill"></i> <i class="fa-solid fa-user"></i>Username</label>
+
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="contra" name="contra" placeholder="Password">
+                                    <label for="pass"><i class="bi bi-lock-fill"></i><i class="fa-solid fa-lock"></i> Password</label>
+
+                                </div>
+                                <div id="formconsole" class="error"></div>
+                                <input type="submit" value="Login" id="boton" class="btn btn-primary " style="background-color: rgb(0,206,129);border-color: rgb(0,206,129)">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
