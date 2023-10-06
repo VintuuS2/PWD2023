b312dce5471e6f07b4f499357f52886f1ec5e461
LautaroGonzalez-FAI2950 <lautarofglez@gmail.com>
2023-10-06 19:25:32 -0300

    Cambios en la vista

--- TP5/Vista/Index.php
+++ TP5/Vista/Index.php
@@ -4,16 +4,16 @@ include_once './Estructura/header.php';
 ?>
 
     <div class="vh-100 w-100 row justify-content-center">
-        <div class="d-flex justify-content-center align-items-center w-50 h-100 bg-gris">
-            <form action="./Accion/translate.php" name="form" id="form" method="post" class="d-flex needs-validation row align-items-center w-50 bg-black p-5 rounded" novalidate>
+        <div class="d-flex justify-content-center align-items-center col-12 col-md-10 col-xl-8 h-100 bg-gris">
+            <form action="./Accion/translate.php" name="form" id="form" method="post" class="d-flex needs-validation row align-items-center col-10 col-md-8 col-xl-6 bg-black p-5 rounded" novalidate>
                 <div class="form-group">
                     <label for="txt" class="form-label mt-3 text-light">Ingrese lo que quiera traducir</label>
-                    <input type="text" class="form-control" maxlength="100" required pattern="[a-zA-ZñÑ0-9 ¡!¿?]+" name="txt" id="txt" placeholder="Ingrese una palabra a traducir" errorVacio="El campo no puede estar vacío" errorPatron="La traducción no admite carácteres especiales">
+                    <input type="text" class="form-control" maxlength="100" required pattern="[a-zA-ZñÑÁÉÍÓÚáéíóú0-9 ¡!¿?,.@$]+" name="txt" id="txt" placeholder="Ingrese una palabra a traducir" errorVacio="El campo no puede estar vacío" errorPatron="La traducción no admite carácteres especiales">
                     <div class="invalid-feedback">
                         El campo no puede estar vacío
                     </div>
                     <div class="valid-feedback">
-                        Se ve bien!
+                        Parece bien!
                     </div>
                 </div>
                 <div class="form-group">
@@ -22,10 +22,20 @@ include_once './Estructura/header.php';
                         <option value="" selected hidden>Traducir del</option>
                         <option value="en">Inglés</option>
                         <option value="es">Español</option>
+                        <option value="de">Alemán</option>
+                        <option value="zh">Chino</option>
+                        <option value="ko">Coreano</option>
+                        <option value="fr">Francés</option>
+                        <option value="it">Italiano</option>
+                        <option value="ja">Japonés</option>
                         <option value="pt">Portugués</option>
+                        <option value="ru">Ruso</option>
                     </select>
                     <div class="invalid-feedback">
                         Ingrese un idioma
+                    </div>
+                    <div class="valid-feedback">
+                        Parece bien!
                     </div>
                 </div>
                 <div class="form-group">
@@ -34,14 +44,24 @@ include_once './Estructura/header.php';
                         <option value="" selected hidden>Traducir al</option>
                         <option value="en">Inglés</option>
                         <option value="es">Español</option>
+                        <option value="de">Alemán</option>
+                        <option value="zh">Chino</option>
+                        <option value="ko">Coreano</option>
+                        <option value="fr">Francés</option>
+                        <option value="it">Italiano</option>
+                        <option value="ja">Japonés</option>
                         <option value="pt">Portugués</option>
+                        <option value="ru">Ruso</option>
                     </select>
                     <div class="invalid-feedback">
                         Ingrese un idioma
                     </div>
+                    <div class="valid-feedback">
+                        Parece bien!
+                    </div>
                 </div>
-                <div class="form-group d-flex justify-content-center">
-                    <input type="submit" class="btn btn-primary mt-3" value="Tonto wn">
+                <div class="form-group d-flex col-12 justify-content-center">
+                    <input type="submit" class="btn btn-primary mt-3" value="Enviar">
                 </div>
             </form>
         </div>
