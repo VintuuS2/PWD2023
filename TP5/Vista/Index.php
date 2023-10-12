5627960b22ad999a140ee65e60b6e0858780c032
Valentin Camusso FAI-3208 <camussovalentin10@gmail.com>
2023-10-12 18:36:08 -0300

    Auto stash before merge of "TP5Prueba1" and "origin/TP5Prueba1"

--- TP5/Vista/Index.php
+++ TP5/Vista/Index.php
@@ -4,70 +4,106 @@ include_once './Estructura/header.php';
 require_once '../Util/vendor/autoload.php';
 
 use Statickidz\GoogleTranslate;
+
 $translator = new GoogleTranslate();
 
 // Variables que contienen textos
-$labelTxt = "Ingrese lo que quiera traducir";
-$labelTxtEN = $translator->translate("es", "en", $labelTxt);
+$lang = isset($_COOKIE['selectedLanguage']) ? $_COOKIE['selectedLanguage'] : 'es';
 
-$txtAreaPlaceholder = "Texto a traducir";
-$txtAreaPlaceholderEN = $translator->translate("es", "en", $txtAreaPlaceholder);
+$labelTxt = [
+    'es' => "Ingrese lo que quiera traducir",
+    'en' => "Enter what you want to translate" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$txtAreaErrorVacio = "El campo no puede estar vacío";
-$txtAreaErrorVacioEN = $translator->translate("es", "en", $txtAreaErrorVacio);
+$labelAreaPlaceHolder = [
+    'es' => "Texto a traducir",
+    'en' => "Text to translate" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$txtAreaErrorPatron = "La traducción no admite carácteres especiales";
-$txtAreaErrorPatronEN = $translator->translate("es", "en", $txtAreaErrorPatron);
+$txtAreaErrorVacio = [
+    'es' => "El campo no puede estar vacío",
+    'en' => "The field cannot be empty" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$labelSrc = "Idioma origen";
-$labelSrcEN = $translator->translate("es", "en", $labelSrc);
+$txtAreaErrorPatron = [
+    'es' => "La traducción no admite carácteres especiales",
+    'en' => "The translation does not support special characters" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$srcText = "Traducir del";
-$srcTextEN = $translator->translate("es", "en", $srcText);
+$labelSrc = [
+    'es' => "Idioma origen",
+    'en' => "Source language" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$labeltgt = "Idioma destino";
-$labeltgtEN = $translator->translate("es", "en", $labeltgt);
+$srcTxt = [
+    'es' => "Traducir del",
+    'en' => "Translate from" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$tgtText = "Traducir al";
-$tgtTextEN = $translator->translate("es", "en", $tgtText);
+$labelTgt = [
+    'es' => "Idioma destino",
+    'en' => "Destination language" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$txtSubmit = "Enviar";
-$txtSubmitEN = "Send"; // no se usa el '$translator->translate' para no hacer muchas request
+$tgtText = [
+    'es' => "Traducir al",
+    'en' => "Translate to" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$invalidFeedbackTxt = "El campo no puede estar vacío";
-$invalidFeedbackTxtEN = "The field cannot be empty"; // no se usa el '$translator->translate' para no hacer muchas request
+$txtSubmit = [
+    'es' => "Enviar",
+    'en' => "Send" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$validFeedback = "Parece estar bien!";
-$validFeedbackEN = "It seems fine!"; // no se usa el '$translator->translate' para no hacer muchas request
+$invalidFeedbackTxt = [
+    'es' => "El campo no puede estar vacío",
+    'en' => "The field cannot be empty" // no se usa el '$translator->translate' para no hacer muchas request
+];
 
-$invalidFeedbackLang = "Seleccione un idioma";
-$invalidFeedbackLangEN = "Select a language"; // no se usa el '$translator->translate' para no hacer muchas request
+$validFeedback = [
+    'es' => "Parece estar bien!",
+    'en' => "It seems fine!" // no se usa el '$translator->translate' para no hacer muchas request
+];
+
+$invalidFeedbackLang = [
+    'es' => "Seleccione un idioma",
+    'en' => "Select a language" // no se usa el '$translator->translate' para no hacer muchas request
+];
 ?>
 
 <div class="vh-100 d-flex justify-content-center">
     <div class="d-flex justify-content-center col-12 col-md-10 col-xl-8 h-100 bg-gris row align-items-start">
         <div class="col-12 d-flex mt-3">
-            <label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-argentina.png"></label>
+            <label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end mx-2" for="language-select"><img id="bandera-idioma" src="./../Imagenes/bandera-<?php echo isset($_COOKIE['selectedLanguage']) && $_COOKIE['selectedLanguage']== 'en' ? 'gran-bretania.png' : 'argentina.png'?>"></label>
             <select class="form-select " id="language-select">
-                <option value="es" selected>Español</option>
-                <option value="en">English</option>
+                <?php
+                if (isset($_COOKIE['selectedLanguage']) && $_COOKIE['selectedLanguage']== 'en') {
+                    echo '<label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-gran-bretania.png"></label>
+                    <option value="es">Español</option>
+                    <option value="en" selected>English</option>';
+                } else {
+                    echo '<label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-argentina.png"></label>
+                    <option value="es" selected>Español</option>
+                    <option value="en">English</option>';
+                }
+                ?>
             </select>
         </div>
         <form action="./Accion/translate.php" name="form" id="form" method="post" class="d-flex needs-validation row align-items-center col-10 col-md-8 col-xl-6 bg-black p-5 rounded" novalidate>
             <div class="form-group">
-                <label for="txt" class="form-label mt-3 text-light" id="labelTxt">Ingrese lo que quiera traducir</label>
-                <textarea rows="3" class="form-control" maxlength="200" required pattern="[a-z]+" name="txt" id="txt" placeholder="Texto a traducir" errorVacio="El campo no puede estar vacío" errorPatron="La traducción no admite carácteres especiales"></textarea> <!-- FALTA ARREGLAR EL PATTERN -->
-                <div class="invalid-feedback" id="invalidFeedbackTxt">
-                    El campo no puede estar vacío
+                <label for="txt" class="form-label mt-3 text-light"><?php echo $labelTxt[$lang] ?></label>
+                <textarea rows="3" class="form-control" maxlength="200" required pattern="[a-z]+" name="txt" id="txt" placeholder="<?php echo $labelAreaPlaceHolder[$lang] ?>" errorVacio="<?php echo $txtAreaErrorVacio[$lang] ?>" errorPatron="<?php echo $txtAreaErrorPatron[$lang] ?>"></textarea> <!-- FALTA ARREGLAR EL PATTERN -->
+                <div class="invalid-feedback">
+                    <?php echo $invalidFeedbackTxt[$lang] ?>
                 </div>
-                <div class="valid-feedback" id="validFeedbackTxt">
-                    Parece estar bien!
+                <div class="valid-feedback">
+                    <?php echo $validFeedback[$lang] ?>
                 </div>
             </div>
             <div class="form-group col-12 col-md-5">
-                <label for="src" class="form-label mt-3 text-light" id="labelSrc">Idioma origen</label>
+                <label for="src" class="form-label mt-3 text-light"><?php echo $labelSrc[$lang] ?></label>
                 <select name="src" id="src" class="form-select" required>
-                    <option value="" selected hidden id="srcText">Traducir del</option>
+                    <option value="" selected hidden><?php echo $srcTxt[$lang] ?></option>
                     <option value="en">English</option>
                     <option value="es">Español</option>
                     <option value="de">Deutsch</option>
@@ -79,20 +115,20 @@ $invalidFeedbackLangEN = "Select a language"; // no se usa el '$translator->tran
                     <option value="pt">Português</option>
                     <option value="ru">Русский</option>
                 </select>
-                <div class="invalid-feedback" id="invalidFeedbackSRC">
-                    Seleccione un idioma
+                <div class="invalid-feedback">
+                    <?php echo $invalidFeedbackLang[$lang] ?>
                 </div>
-                <div class="valid-feedback" id="validFeedbackSRC">
-                    Parece estar bien!
+                <div class="valid-feedback">
+                    <?php echo $validFeedback[$lang] ?>
                 </div>
             </div>
             <div class="col-12 col-md-2 d-flex justify-content-center">
                 <button type="button" id="btn-itercambiar-idiomas" class="btn btn-light mt-5 px-4"><i class="fa-solid fa-arrow-right-arrow-left"></i></button>
             </div>
             <div class="form-group col-12 col-md-5">
-                <label for="tgt" class="form-label mt-3 text-light" id="labelTgt">Idioma destino</label>
+                <label for="tgt" class="form-label mt-3 text-light"><?php echo $labelTgt[$lang] ?></label>
                 <select name="tgt" id="tgt" class="form-select" required>
-                    <option value="" selected hidden id="tgtText">Traducir al</option>
+                    <option value="" selected hidden><?php echo $tgtText[$lang] ?></option>
                     <option value="en">English</option>
                     <option value="es">Español</option>
                     <option value="de">Deutsch</option>
@@ -104,78 +140,21 @@ $invalidFeedbackLangEN = "Select a language"; // no se usa el '$translator->tran
                     <option value="pt">Português</option>
                     <option value="ru">Русский</option>
                 </select>
-                <div class="invalid-feedback" id="invalidFeedbackTGT">
-                    Seleccione un idioma
+                <div class="invalid-feedback">
+                    <?php echo $invalidFeedbackLang[$lang] ?>
                 </div>
-                <div class="valid-feedback" id="validFeedbackTGT">
-                    Parece estar bien!
+                <div class="valid-feedback">
+                    <?php echo $validFeedback[$lang] ?>
                 </div>
             </div>
             <div class="form-group d-flex col-12 justify-content-center">
-                <input type="submit" id="submit" class="btn btn-primary mt-3" value="Enviar">
+                <input type="submit" id="submit" class="btn btn-primary mt-3" value="<?php echo $txtSubmit[$lang] ?>">
             </div>
         </form>
     </div>
 </div>
 <script src="./JS/validador.js"></script>
 <script src="./JS/funciones.js"></script>
-<script>
-    // Este script tiene que estar tiene que estar si o si en este archivo index porque si esta aparte los mensajes se muestran mal
-    var select = $('#language-select');
-    var labelTxt = $('#labelTxt');
-    var txtArea = $('#txt');
-    var labelSrc = $('#labelSrc');
-    var srcText = $('#srcText');
-    var labelTgt = $('#labelTgt');
-    var tgtText = $('#tgtText');
-    var submit = $('#submit');
-    var bandera = $('#bandera-idioma');
-    var invalidFeedbackTxt = $('#invalidFeedbackTxt');
-    var validFeedbackTxt = $('#validFeedbackTxt');
-    var invalidFeedbackSRC = $('#invalidFeedbackSRC');
-    var validFeedbackSRC = $('#validFeedbackSRC');
-    var invalidFeedbackTGT = $('#invalidFeedbackTGT');
-    var validFeedbackTGT = $('#validFeedbackTGT');
-    
-    select.on('change', function() {
-        var selectedLanguage = select.val();
-        if (selectedLanguage === 'es') {
-            labelTxt.text("<?php echo $labelTxt; ?>");
-            txtArea.attr("placeholder" , "<?php echo $txtAreaPlaceholder; ?>");
-            txtArea.attr("errorVacio" , "<?php echo $txtAreaErrorVacio; ?>");
-            txtArea.attr("errorPatron" , "<?php echo $txtAreaErrorPatron; ?>");
-            labelSrc.text("<?php echo $labelSrc; ?>");
-            srcText.text("<?php echo $srcText; ?>");
-            labelTgt.text("<?php echo $labeltgt; ?>");
-            tgtText.text("<?php echo $tgtText; ?>");
-            submit.attr("value" , "<?php echo $txtSubmit; ?>");
-            bandera.attr("src" , "./../Imagenes/bandera-argentina.png");
-            invalidFeedbackTxt.text("<?php echo $invalidFeedbackTxt; ?>");
-            validFeedbackTxt.text("<?php echo $validFeedback; ?>");
-            invalidFeedbackSRC.text("<?php echo $invalidFeedbackLang; ?>");
-            validFeedbackSRC.text("<?php echo $validFeedback; ?>");
-            invalidFeedbackTGT.text("<?php echo $invalidFeedbackLang; ?>");
-            validFeedbackTGT.text("<?php echo $validFeedback; ?>");
-        } else {
-            labelTxt.text("<?php echo $labelTxtEN; ?>");
-            txtArea.attr("placeholder" , "<?php echo $txtAreaPlaceholderEN; ?>");
-            txtArea.attr("errorVacio" , "<?php echo $txtAreaErrorVacioEN; ?>");
-            txtArea.attr("errorPatron" , "<?php echo $txtAreaErrorPatronEN; ?>");
-            labelSrc.text("<?php echo $labelSrcEN; ?>");
-            srcText.text("<?php echo $srcTextEN; ?>");
-            labelTgt.text("<?php echo $labeltgtEN; ?>");
-            tgtText.text("<?php echo $tgtTextEN; ?>");
-            submit.attr("value" , "<?php echo $txtSubmitEN; ?>");
-            bandera.attr("src" , "./../Imagenes/bandera-gran-bretania.png");
-            invalidFeedbackTxt.text("<?php echo $invalidFeedbackTxtEN; ?>");
-            validFeedbackTxt.text("<?php echo $validFeedbackEN; ?>");
-            invalidFeedbackSRC.text("<?php echo $invalidFeedbackLangEN; ?>");
-            validFeedbackSRC.text("<?php echo $validFeedbackEN; ?>");
-            invalidFeedbackTGT.text("<?php echo $invalidFeedbackLangEN; ?>");
-            validFeedbackTGT.text("<?php echo $validFeedbackEN; ?>");
-        }
-    });
-</script>
 <?php
 include_once './Estructura/footer.php';
 ?>
--- TP5/Vista/pruebaIndex.php
+++ TP5/Vista/pruebaIndex.php
@@ -4,10 +4,11 @@ include_once './Estructura/header.php';
 require_once '../Util/vendor/autoload.php';
 
 use Statickidz\GoogleTranslate;
+
 $translator = new GoogleTranslate();
 
 // Variables que contienen textos
-$lang = isset($_COOKIE['selectedLanguage']) ? $_COOKIE['selectedLanguage']: 'es';
+$lang = isset($_COOKIE['selectedLanguage']) ? $_COOKIE['selectedLanguage'] : 'es';
 
 $datos = data_submitted();
 
@@ -17,7 +18,7 @@ $labelTxt = [
 ];
 
 //$labelTxt['en'] = $translator->translate('es','en', 'Ingrese lo que quiera traducir');
- 
+
 $labelAreaPlaceHolder = [
     'es' => "Texto a traducir",
     'en' => "Text to translate"
@@ -77,24 +78,24 @@ $invalidFeedbackLang = [
 <div class="vh-100 d-flex justify-content-center">
     <div class="d-flex justify-content-center col-12 col-md-10 col-xl-8 h-100 bg-gris row align-items-start">
         <div class="col-12 d-flex mt-3">
-            <label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-<?php echo $_COOKIE['selectedLanguage']=='es'? 'argentina.png' : 'gran-bretania.png'?>"></label>
+            <label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end mx-2" for="language-select"><img id="bandera-idioma" src="./../Imagenes/bandera-<?php echo isset($_COOKIE['selectedLanguage']) && $_COOKIE['selectedLanguage'] == 'en' ? 'gran-bretania.png' : 'argentina.png' ?>"></label>
             <select class="form-select " id="language-select">
                 <?php
-                if ($_COOKIE['selectedLanguage']=='es'){
-                    echo '<label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-argentina.png"></label>
-                    <option value="es" selected>Español</option>
-                    <option value="en">English</option>';
-                }  else {
+                if (isset($_COOKIE['selectedLanguage']) && $_COOKIE['selectedLanguage'] == 'en') {
                     echo '<label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-gran-bretania.png"></label>
                     <option value="es">Español</option>
                     <option value="en" selected>English</option>';
+                } else {
+                    echo '<label class="col-xl-10 col-md-10 col-8 d-flex justify-content-end" for="language-select"><img id="bandera-idioma" class="mx-2" src="./../Imagenes/bandera-argentina.png"></label>
+                    <option value="es" selected>Español</option>
+                    <option value="en">English</option>';
                 }
                 ?>
             </select>
         </div>
         <div class="d-flex justify-content-center resultado">
-            <?php 
-            if (isset($datos['txt']) && isset($datos['src']) && isset($datos['tgt'])){
+            <?php
+            if (isset($datos['txt']) && isset($datos['src']) && isset($datos['tgt'])) {
                 $txt = $datos['txt'];
                 $tgt = $datos['tgt'];
                 $src = $datos['src'];
@@ -106,39 +107,39 @@ $invalidFeedbackLang = [
 
                 $translate = $translator->translate($src, $tgt, $txt);
 
-                $url = $web."?".http_build_query([
+                $url = $web . "?" . http_build_query([
                     'ie' => 'UTF-8',
                     'client' => 'gtx',
                     'q' => $translate,
                     'tl' => $tgt
                 ]);
-            
+
                 $file = file_get_contents($url);
-            
-                $audio = $base64.base64_encode($file);
-            
-                $msg = "<audio id='m' src='".$audio."'></audio>\n<button id='btnAudio' class='btn btn-primary mt-3'><i class='fas fa-volume-high'></i></button>";
+
+                $audio = $base64 . base64_encode($file);
+
+                $msg = "<audio id='m' src='" . $audio . "'></audio>\n<button id='btnAudio' class='btn btn-primary mt-3'><i class='fas fa-volume-high'></i></button>";
                 echo $msg;
             } else {
                 echo "";
             }
             ?>
         </div>
-        <form action="<?php echo $_SERVER['PHP_SELF']?>" name="form" id="form" method="post" class="d-flex needs-validation row align-items-center col-10 col-md-8 col-xl-6 bg-black p-5 rounded" novalidate>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" name="form" id="form" method="post" class="d-flex needs-validation row align-items-center col-10 col-md-8 col-xl-6 bg-black p-5 rounded" novalidate>
             <div class="form-group">
-                <label for="txt" class="form-label mt-3 text-light" id="labelTxt"><?php echo $labelTxt[$lang]?></label>
-                <textarea rows="3" class="form-control" maxlength="200" required pattern="[a-z]+" name="txt" id="txt" placeholder="<?php echo $labelAreaPlaceHolder[$lang]?>" errorVacio="El campo no puede estar vacío" errorPatron="La traducción no admite carácteres especiales"></textarea> <!-- FALTA ARREGLAR EL PATTERN -->
-                <div class="invalid-feedback" id="invalidFeedbackTxt">
-                    El campo no puede estar vacío
+                <label for="txt" class="form-label mt-3 text-light"><?php echo $labelTxt[$lang] ?></label>
+                <textarea rows="3" class="form-control" maxlength="200" required pattern="[a-z]+" name="txt" id="txt" placeholder="<?php echo $labelAreaPlaceHolder[$lang] ?>" errorVacio="<?php echo $txtAreaErrorVacio[$lang] ?>" errorPatron="<?php echo $txtAreaErrorPatron[$lang] ?>"></textarea> <!-- FALTA ARREGLAR EL PATTERN -->
+                <div class="invalid-feedback">
+                    <?php echo $invalidFeedbackTxt[$lang] ?>
                 </div>
-                <div class="valid-feedback" id="validFeedbackTxt">
-                    Parece estar bien!
+                <div class="valid-feedback">
+                    <?php echo $validFeedback[$lang] ?>
                 </div>
             </div>
             <div class="form-group col-12 col-md-5">
-                <label for="src" class="form-label mt-3 text-light" id="labelSrc">Idioma origen</label>
+                <label for="src" class="form-label mt-3 text-light"><?php echo $labelSrc[$lang] ?></label>
                 <select name="src" id="src" class="form-select" required>
-                    <option value="" selected hidden id="srcText">Traducir del</option>
+                    <option value="" selected hidden><?php echo $srcTxt[$lang] ?></option>
                     <option value="en">English</option>
                     <option value="es">Español</option>
                     <option value="de">Deutsch</option>
@@ -150,20 +151,20 @@ $invalidFeedbackLang = [
                     <option value="pt">Português</option>
                     <option value="ru">Русский</option>
                 </select>
-                <div class="invalid-feedback" id="invalidFeedbackSRC">
-                    Seleccione un idioma
+                <div class="invalid-feedback">
+                    <?php echo $invalidFeedbackLang[$lang] ?>
                 </div>
-                <div class="valid-feedback" id="validFeedbackSRC">
-                    Parece estar bien!
+                <div class="valid-feedback">
+                    <?php echo $validFeedback[$lang] ?>
                 </div>
             </div>
             <div class="col-12 col-md-2 d-flex justify-content-center">
                 <button type="button" id="btn-itercambiar-idiomas" class="btn btn-light mt-5 px-4"><i class="fa-solid fa-arrow-right-arrow-left"></i></button>
             </div>
             <div class="form-group col-12 col-md-5">
-                <label for="tgt" class="form-label mt-3 text-light" id="labelTgt">Idioma destino</label>
+                <label for="tgt" class="form-label mt-3 text-light"><?php echo $labelTgt[$lang] ?></label>
                 <select name="tgt" id="tgt" class="form-select" required>
-                    <option value="" selected hidden id="tgtText">Traducir al</option>
+                    <option value="" selected hidden><?php echo $tgtText[$lang] ?></option>
                     <option value="en">English</option>
                     <option value="es">Español</option>
                     <option value="de">Deutsch</option>
@@ -175,15 +176,15 @@ $invalidFeedbackLang = [
                     <option value="pt">Português</option>
                     <option value="ru">Русский</option>
                 </select>
-                <div class="invalid-feedback" id="invalidFeedbackTGT">
-                    Seleccione un idioma
+                <div class="invalid-feedback">
+                    <?php echo $invalidFeedbackLang[$lang] ?>
                 </div>
-                <div class="valid-feedback" id="validFeedbackTGT">
-                    Parece estar bien!
+                <div class="valid-feedback">
+                    <?php echo $validFeedback[$lang] ?>
                 </div>
             </div>
             <div class="form-group d-flex col-12 justify-content-center">
-                <input type="submit" id="submit" class="btn btn-primary mt-3" value="Enviar">
+                <input type="submit" id="submit" class="btn btn-primary mt-3" value="<?php echo $txtSubmit[$lang] ?>">
             </div>
         </form>
     </div>
@@ -192,12 +193,12 @@ $invalidFeedbackLang = [
 <script src="./JS/funciones.js"></script>
 <script>
     var resultado = $('.resultado');
-    console.log(resultado.innerHTML);
     var form = $('#form');
-    if (resultado.is(':empty')){
+    console.log(resultado.text().trim()); // Esto mostrará el contenido de texto en la consola
+    if (resultado.text().trim() === '') {
         form.css("margin-top", "-400px");
     } else {
-        form.css("margin-top", "100px");
+        form.css("margin-top", "20px");
     }
 </script>
 <?php
