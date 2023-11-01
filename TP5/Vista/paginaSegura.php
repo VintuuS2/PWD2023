364c04bbdff6580883b9f684cc453de528e4f808
Emiliano Lopez FAI [3357] <emiliano.lopez@est.fi.uncoma.edu.ar>
2023-11-01 16:33:47 -0300

    Detalles en el login y paginaSegura.php para pruebas

---
+++ TP5/Vista/paginaSegura.php
@@ -0,0 +1,15 @@
+<?php
+$tituloPagina = "TP5-Login";
+include_once "../../configuracionProyecto.php";
+include_once "../../TP5/Vista/Estructura/header.php";
+?>
+<div class="d-flex justify-content-center align-items-center ">
+    <div class="d-flex justify-content-center bg-gris row col-12 col-md-8 row position-relative h-100 align-items-center min-vh-100">
+        <div class="bg-black p-5 rounded-5 text-white col-12 col-sm-8 col-md-8 col-lg-6 col-xl-4">
+            <h3>Loggeado Correctamente</h3>
+        </div>
+    </div>
+</div>
+<?php
+include_once "../../vista/estructura/footer.php";
+?>
