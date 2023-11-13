dac8eff69d2cf65e7182717abdd16c76afe3702f
Lautaro Gonzalez <lautarofglez@gmail.com>
2023-11-12 23:48:21 -0300

    Prueba de cartas
    En el index le puse unas ilusiones opticas owo

--- TPFinal/Vista/index.php
+++ TPFinal/Vista/index.php
@@ -4,15 +4,12 @@ include_once "../../configuracionProyecto.php";
 include_once "../configuracion.php";
 include_once "./Estructura/header.php";
 $sesion = new Session();
-if ($sesion->validar()){
-  setcookie("login",1);
+if ($sesion->validar()) {
+  setcookie("login", 1);
 } else {
-  setcookie("login",0);
+  setcookie("login", 0);
 }
-//include_once "./Estructura/navbar.php";
-//include_once "./Estructura/otroNavbar.php";
 include_once "./Estructura/ultimoNav.php";
-//include_once "./Estructura/sidebarReal.php";
 ?>
 <div class="min-vh-100 d-flex justify-content-center">
   <div class="col-12 col-lg-9 bg-body-tertiary h-100 min-vh-100">
@@ -27,15 +24,44 @@ include_once "./Estructura/ultimoNav.php";
     </div>
     <!-- </Banner> --->
     <div class="border-top w-75 mx-auto my-5">
-      
+
     </div>
     <!-- <Catálogo de opciones recomendadas> --->
     <div class="w-75 mt-5 p-5 bg-body-secondary mx-auto">
-      <div class="text-center">
+      <div class="text-center mb-4">
         <h3>Productos destacados</h3>
       </div>
-      <div>
-
+      <div class="d-flex justify-content-around align-items-center">
+        <!-- <1erItem> --->
+        <div class="card" style="width: 18rem;">
+          <img src="https://i.pinimg.com/originals/1f/bf/18/1fbf1891190c7fe0a89a31009609bbd0.gif" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!-- </1erItem> --->
+        <!-- <2doItem> --->
+        <div class="card" style="width: 18rem;">
+          <img src="https://i.pinimg.com/originals/1f/bf/18/1fbf1891190c7fe0a89a31009609bbd0.gif" class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!-- </2doItem> --->
+        <!-- <3erItem> --->
+        <div class="card" style="width: 18rem;">
+          <img src="https://i.pinimg.com/originals/1f/bf/18/1fbf1891190c7fe0a89a31009609bbd0.gif" class="card-img-top" alt="..." style="height: 100%; width: 100%;">
+          <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!-- </3erItem> --->
       </div>
     </div>
     <!-- </Catálogo de opciones recomendadas> --->
